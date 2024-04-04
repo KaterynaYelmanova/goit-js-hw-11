9134f68198fc3acd536c438269eb1ddb8cfb389c
KaterynaYelmanova <148703875+KaterynaYelmanova@users.noreply.github.com>
2024-04-04 10:06:06 +0300

    js-11end

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,15 +1,19 @@
 import { galleryList } from '../main';
-import { query } from '../main';
 
 export function fetchImages(query) {
   query = encodeURIComponent(query);
 
   return fetch(
     `https://pixabay.com/api/?key=43144570-5608d834234b71965ee211368&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
-  ).then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error(response.status);
-  });
+  )
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(response.status);
+      }
+    })
+    .catch(error => {
+      console.error('Помилка запиту:', error);
+    });
 }
--- src/main.js
+++ src/main.js
@@ -6,18 +6,21 @@ import { fetchImages } from './js/pixabay-api';
 import { renderImages } from './js/render-functions';
 
 export const galleryList = document.querySelector('ul.gallery');
-export let query = '';
+let query = '';
+
+const displayLoader = () => {
+  galleryList.innerHTML = '<span class="loader"></span>';
+};
 
 const inputQuery = document.getElementById('search-input');
 inputQuery.addEventListener('input', e => {
   query = inputQuery.value.trim();
-  galleryList.innerHTML = '';
 });
 
 const searchButton = document.getElementById('search-button');
 searchButton.addEventListener('click', () => {
-  if (query) {
-    galleryList.innerHTML = '<span class="loader"></span>';
+  if (query && query.length >= 3) {
+    displayLoader();
 
     fetchImages(query)
       .then(data => renderImages(data))
@@ -29,5 +32,14 @@ searchButton.addEventListener('click', () => {
         });
       });
     inputQuery.value = '';
+  } else {
+    alert('Please enter a valid search query with at least 3 characters.');
+  }
+});
+
+inputQuery.addEventListener('keypress', e => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    searchButton.click();
   }
 });
